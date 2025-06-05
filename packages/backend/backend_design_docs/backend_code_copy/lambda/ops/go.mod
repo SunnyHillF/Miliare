681a456228f7e1b350e70f11ee2abbb7711c734e
richardstanley <rstanley943@gmail.com>
2025-06-05 10:15:24 -0700

    Fixing the build issue

--- packages/backend/backend_design_docs/backend_code_copy/lambda/ops/go.mod
+++
@@ -1,5 +0,0 @@
-module ops
-
-go 1.24.3
-
-require github.com/aws/aws-lambda-go v1.49.0
--- packages/backend/backend_design_docs/frontend_code/components/ui/label.tsx
+++
@@ -1,6 +0,0 @@
-import clsx from 'clsx'
-import type { LabelHTMLAttributes } from 'react'
-
-export function Label({ className, ...props }: LabelHTMLAttributes<HTMLLabelElement>) {
-  return <label className={clsx('text-sm font-medium', className)} {...props} />
-}
